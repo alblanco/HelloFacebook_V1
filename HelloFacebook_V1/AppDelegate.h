4fff6a13c4bdad65bb5dc38a291f5f1789e8db14
Al <blanco.artist@gmail.com>
2012-10-26 11:38:32 -0400

    New Facebook Test

---
+++ HelloFacebook_V1/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  HelloFacebook_V1
+//
+//  Created by Alberto Blanco on 10/26/12.
+//  Copyright (c) 2012 Alberto Blanco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//@interface AppDelegate : UIResponder <UIApplicationDelegate>
+//
+//@property (strong, nonatomic) UIWindow *window;
+//
+//@end
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
---
+++ HelloFacebook_V1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloFacebook_V1
+//
+//  Created by Alberto Blanco on 10/26/12.
+//  Copyright (c) 2012 Alberto Blanco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
